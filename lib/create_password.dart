9be4a5545f06d9c934a6af2e6b48421999e01a54
angyk <angelak1516@gmail.com>
2024-04-05 17:24:09 -0400

    reformatting

--- lib/create_password.dart
+++ lib/create_password.dart
@@ -1,8 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
-import 'firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:password_manager/document_id.dart';
 import 'package:password_manager/password_widget.dart';
